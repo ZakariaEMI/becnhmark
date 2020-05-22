f66beec6d6b487c3a007b41672a8a5dd191144c4
El Meliani Zakaria <zakaria.elmeliani@sofrecom.com>
2020-05-22 13:38:13 +0000

    ZakariaEMI: Add apache commons example

--- src/main/java/com/benchmark/apache/ApacheCommonsCodec.java
+++ src/main/java/com/benchmark/apache/ApacheCommonsCodec.java
@@ -6,16 +6,15 @@ import org.apache.commons.codec.binary.Base64;
  * @author ZakariaEMI
  * How to use some methods of Apache Common Codec
  */
-public class ApacheCommonsCodec {
+public final class ApacheCommonsCodec {
 
-    public static void main(String[] args) {
+    /**
+     * Test some examples of apache common codec
+     */
+    public static void encode() {
         Base64 lBase64 = new Base64();
         String lStringToEncode = "First String";
         String lEncodedVersion = new String(lBase64.encode(lStringToEncode.getBytes()));
         String lDecodedVersion = new String(lBase64.decode(lEncodedVersion.getBytes()));
-
-        System.out.println(lStringToEncode);
-        System.out.println(lEncodedVersion);
-        System.out.println(lDecodedVersion);
     }
 }
